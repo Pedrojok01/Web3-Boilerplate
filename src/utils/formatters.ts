edbf4975be730131b86167bc3c1f0363ad4c1ab4
Pedrojok01 <pedrojok@pm.me>
2022-07-30 17:51:58 +0700

    Convert formatter to ts file

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -22,14 +22,14 @@ export const c2 = new Intl.NumberFormat("en-us", {
  * @param {number} n number of chars to keep at front/end
  * @returns {string}
  */
-export const getEllipsisTxt = (str, n = 6) => {
+export const getEllipsisTxt = (str: string, n = 6) => {
   if (str) {
     return `${str.slice(0, n)}...${str.slice(str.length - n)}`;
   }
   return "";
 };
 
-export const tokenValue = (value, decimals) => (decimals ? value / Math.pow(10, decimals) : value);
+export const tokenValue = (value: number, decimals: number) => (decimals ? value / Math.pow(10, decimals) : value);
 
 /**
  * Return a formatted string with the symbol at the end
@@ -38,4 +38,5 @@ export const tokenValue = (value, decimals) => (decimals ? value / Math.pow(10, 
  * @param {string} symbol token symbol
  * @returns {string}
  */
-export const tokenValueTxt = (value, decimals, symbol) => `${n4.format(tokenValue(value, decimals))} ${symbol}`;
+export const tokenValueTxt = (value: number, decimals: number, symbol: string) =>
+  `${n4.format(tokenValue(value, decimals))} ${symbol}`;
