2c67264000803d3ef5c87dad24f7609be8c5be92
unknown <pedrojok@pm.me>
2022-12-03 15:56:36 +0700

    Add extra node access

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,5 +1,9 @@
 import type { AddEthereumChainParameter } from "@web3-react/types";
 
+const infuraKey = process.env.REACT_APP_INFURA_KEY;
+const alchemyKey = process.env.REACT_APP_INFURA_KEY;
+const pocketNetwork = process.env.REACT_APP_POCKET_KEY;
+
 const ETH: AddEthereumChainParameter["nativeCurrency"] = {
   name: "Ether",
   symbol: "ETH",
@@ -19,6 +23,7 @@ const BSC: AddEthereumChainParameter["nativeCurrency"] = {
 };
 
 interface BasicChainInformation {
+  chainId: string;
   urls: string[];
   name: string;
 }
@@ -65,48 +70,71 @@ export const CHAINS: {
   [chainId: number]: BasicChainInformation | ExtendedChainInformation;
 } = {
   1: {
+    chainId: "1",
     urls: [
-      `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
-      `https://eth-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
+      infuraKey ? `https://mainnet.infura.io/v3/${infuraKey}` : "",
+      alchemyKey ? `https://eth-mainnet.g.alchemy.com/v2/${alchemyKey}` : "",
+      pocketNetwork ? `https://eth-mainnet.gateway.pokt.network/v1/lb/${pocketNetwork}` : "",
+      "https://rpc.ankr.com/eth",
       "https://cloudflare-eth.com"
-    ].filter((url) => url !== undefined),
+    ].filter((url) => url !== ""),
     name: "Mainnet",
     nativeCurrency: ETH,
     blockExplorerUrls: ["https://etherscan.io"]
   },
   5: {
-    urls: [`https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`].filter((url) => url !== undefined),
+    chainId: "5",
+    urls: [
+      infuraKey ? `https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}` : "",
+      alchemyKey ? `https://eth-goerli.g.alchemy.com/v2/${alchemyKey}` : "",
+      pocketNetwork ? `https://eth-goerli.gateway.pokt.network/v1/lb/${pocketNetwork}` : "",
+      "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+    ].filter((url) => url !== ""),
     name: "Görli",
     nativeCurrency: ETH,
     blockExplorerUrls: ["https://goerli.etherscan.io/"]
   },
   // Polygon
   137: {
+    chainId: "137",
     urls: [
-      `https://polygon-mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+      infuraKey ? `https://polygon-mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}` : "",
+      alchemyKey ? `https://polygon-mainnet.g.alchemy.com/v2/${alchemyKey}` : "",
+      pocketNetwork ? `https://poly-mainnet.gateway.pokt.network/v1/lb/${pocketNetwork}` : "",
       "https://polygon-rpc.com"
-    ].filter((url) => url !== undefined),
+    ].filter((url) => url !== ""),
     name: "Polygon Mainnet",
     nativeCurrency: MATIC,
     blockExplorerUrls: ["https://polygonscan.com"]
   },
   80001: {
-    urls: [`https://polygon-mumbai.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`].filter(
-      (url) => url !== undefined
-    ),
+    chainId: "80001",
+    urls: [
+      infuraKey ? `https://polygon-mumbai.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}` : "",
+      alchemyKey ? `https://polygon-mumbai.g.alchemy.com/v2/${alchemyKey}` : "",
+      "https://rpc-mumbai.maticvigil.com"
+    ].filter((url) => url !== ""),
     name: "Polygon Mumbai",
     nativeCurrency: MATIC,
     blockExplorerUrls: ["https://mumbai.polygonscan.com"]
   },
   // BSC
   56: {
-    urls: ["https://bsc-dataseed.binance.org/"].filter((url) => url !== undefined),
+    chainId: "56",
+    urls: [
+      pocketNetwork ? `https://bsc-mainnet.gateway.pokt.network/v1/lb/${pocketNetwork}` : "",
+      "https://bsc-dataseed.binance.org/",
+      "https://rpc.ankr.com/bsc"
+    ].filter((url) => url !== ""),
     name: "BNB Chain",
     nativeCurrency: BSC,
     blockExplorerUrls: ["https://bscscan.com/"]
   },
   97: {
-    urls: ["https://data-seed-prebsc-1-s1.binance.org:8545/"].filter((url) => url !== undefined),
+    chainId: "97",
+    urls: ["https://data-seed-prebsc-1-s1.binance.org:8545/", "https://data-seed-prebsc-1-s3.binance.org:8545/"].filter(
+      (url) => url !== ""
+    ),
     name: "BNB Testnet",
     nativeCurrency: BSC,
     blockExplorerUrls: ["https://testnet.bscscan.com/"]
@@ -119,6 +147,19 @@ export const URLS: { [chainId: number]: string[] } = Object.keys(CHAINS).reduce<
 
     if (validURLs.length) {
       accumulator[Number(chainId)] = validURLs;
+    }
+
+    return accumulator;
+  },
+  {}
+);
+
+export const CHAINIDs: { [chainId: number]: string } = Object.keys(CHAINS).reduce<{ [chainId: number]: string }>(
+  (accumulator, chainId) => {
+    const validCHAINIDs: string = CHAINS[Number(chainId)].chainId;
+
+    if (validCHAINIDs) {
+      accumulator[Number(chainId)] = validCHAINIDs;
     }
 
     return accumulator;
